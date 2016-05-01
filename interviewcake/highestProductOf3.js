db2216a4a6c7758a91bab1b46f4ad07b01e67cdc
John Domingo <johnwill.domingo@gmail.com>
2016-05-01 00:28:57 -0700

    Outline pseudocode to solution

--- interviewcake/highestProductOf3.js
+++ interviewcake/highestProductOf3.js
@@ -6,3 +6,11 @@
  * The input arrayOfInts will always have at least three integers.
  *
  */
+
+/* I am given an array containing integers and I am supposed to return the largest product of three elements (an integer). If there are only positive integers, I simply have to sort the array and return the product of the largest three. If there are negative integers, I need to take the max of a) the products of the largest three and b) the product of the largest integer and the two smallest integers (in case there are the very small negative numbers that result in a large positive). Another edge case is when all elements in the array are negative numbers but the algorithm I proposed handles this. */
+
+// findHighestProductOf3 takes an array
+
+  // sort array
+
+  // return max of top three ints or bottom two ints and top int

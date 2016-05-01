9f839e5d0d9a8ef4d5a62461f8098ac964042dc3
John Domingo <johnwill.domingo@gmail.com>
2016-04-30 18:08:46 -0700

    Implement sub-optimal quadratic time solution

---
+++ interviewcake/productOfAllOtherNumbers.js
@@ -0,0 +1,46 @@
+/*
+ * Product of All Other Numbers
+ *
+ * You have an array of integers, and for each index you want to find the
+ * product of every integer except the integer at that index.
+ * Write a function getProductsOfAllIntsExceptAtIndex() that takes an array of
+ * integers and returns an array of the products.
+ *
+ * For example, given:
+ * [1, 7, 3, 4]
+ *
+ * your function would return:
+ * [84, 12, 28, 21]
+ *
+ * by calculating:
+ * [7*3*4, 1*3*4, 1*7*4, 1*7*3]
+ *
+ * Do not use division in your solution.
+ *
+*/
+
+/*---------------------------------*/
+/*      SUB-OPTIMAL SOLUTION       */
+/*     O(n^2) time, O(n) space     */
+/*---------------------------------*/
+
+var getProductsOfAllIntsExceptAtIndex = function (arr) {
+  var result = [];
+  var pastProducts = 1;
+  var products;
+
+  for (var i = 0, l = arr.length; i < l; i++) {
+    products = 1;
+    for (var j = i + 1; j < l; j++) {
+      products *= arr[j];
+    }
+    result.push(products * pastProducts);
+    pastProducts *= arr[i];
+  }
+
+  return result;
+};
+
+console.log(getProductsOfAllIntsExceptAtIndex([1, 7, 3, 4]));
+console.log(getProductsOfAllIntsExceptAtIndex([100]));
+console.log(getProductsOfAllIntsExceptAtIndex([3, 5, 6, 0]));

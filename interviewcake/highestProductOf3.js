3929f8fe42197ac62bf107212de6b84dfd7aade6
John Domingo <johnwill.domingo@gmail.com>
2016-05-02 09:05:42 -0700

    Refactor optimal solution; does not cover edge cases

--- interviewcake/highestProductOf3.js
+++ interviewcake/highestProductOf3.js
@@ -30,8 +30,26 @@ console.log('findHighestProductOf3Sub( [-10, -10, 1, 3, 2] ) == 120 :', findHigh
 /*     O(n) time, O(n) space     */
 /*-------------------------------*/
 
-var findHighestProductOf3 = function (arrayOfInts) {  
+var findHighestProductOf3 = function (arrayOfInts) {
+  var highestProductOfThree = 1;
+  var lowest = Math.min(arrayOfInts[0], arrayOfInts[1]);
+  var lowestProductOf2 = arrayOfInts[0] * arrayOfInts[1];
+  var highest = Math.max(arrayOfInts[0], arrayOfInts[1]);;
+  var highestProductOf2 = lowestProductOf2;
+
+  for (var i = 2, l = arrayOfInts.length; i < l; i++) {
+    lowest = Math.min(lowest, arrayOfInts[i]);
+    lowestProductOf2 = Math.min(lowestProductOf2, lowest * arrayOfInts[i]);
+
+    highest = Math.max(highest, arrayOfInts[i]);
+    highestProductOf2 = Math.max(highestProductOf2, highest * arrayOfInts[i]);
+
+    highestProductOfThree = Math.max(highestProductOfThree, arrayOfInts[i] * Math.max(lowestProductOf2, highestProductOf2));
+  }
+
+  return highestProductOfThree;
 }
 
-console.log('findHighestProductOf3( [6, 5, 4, 3, 2, 1] ) == 120 :', findHighestProductOf3([6, 5, 4, 3, 2, 1]) == 120);
-console.log('findHighestProductOf3( [-10, -10, 1, 3, 2] ) == 120 :', findHighestProductOf3([-10, -10, 1, 3, 2]) == 300);
+console.log('findHighestProductOf3( [6, 5, 4, 3, 2, 1] ) == 120 :', findHighestProductOf3([6, 5, 4, 3, 2, 1]));
+console.log('findHighestProductOf3( [1, 6, 3, 2, 4, 5] ) == 120 :', findHighestProductOf3([1, 6, 3, 2, 4, 5]));
+console.log('findHighestProductOf3( [-10, -10, 1, 3, 2] ) == 300 :', findHighestProductOf3([-10, -10, 1, 3, 2]));

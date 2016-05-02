336ceb4f8cbf0682048f72acfbe72bf7f1bdfefd
John Domingo <johnwill.domingo@gmail.com>
2016-05-02 11:54:17 -0700

    Clean up pseudocode

--- pramp/getDifferentNumber.js
+++ pramp/getDifferentNumber.js
@@ -10,35 +10,17 @@
  * Analyze the runtime and space complexity of your solution.
  */
 
-/*
-
-If I am to be given an array of unique non-negative integers, the brute force approach would be to go through the array and look for 1. If it exists, iterate through array again to find 2. If 2 exists, keep incrementing the target and looping through the array until the target is not found. Of course, the time complexity here is exponential or rather O(n^target).
-
-A slightly improved version is to loop through the array and create a dictionary of all the integers. A second loop then goes through and incrementally checks if each non-negative integeger exists in the dictionary. Simply return the first one that does not exist. The time (and space) complexity here is O(n) linear because we iterate through the entire array twice to find the missing number.
-
-*/
-
-// getDifferentNumber takes an arr
 var getDifferentNumber = function (arr) {
   var len = arr.length;
-  // return null if arr is empty
-  // check if arr contains all possible non-negative integers already
   if (!len || len === Infinity + 1) {
-    // if so return null
     return null;
   }
 
-  // create hash
   var hash = {};
-  // loop through arr to map each value to hash
   for (var i = 0; i < len; i++) {
-    // value of each key is arbitrary
     hash[i] = 1;
   }
-  // loop through arr again, this time looping up to n + 1
   for (var diffNum = 0; diffNum <= len; diffNum++) {
-    // based on pigeonhole principle, if we know one int will not belong in arr
-    // simply return int if it is not hash
     if (!hash[diffNum]) {
       return diffNum;
     }

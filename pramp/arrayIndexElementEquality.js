19c97343ae0116d6a511c78dfa9dfe1c3e8e51dc
John Domingo <johnwill.domingo@gmail.com>
2016-05-03 10:54:47 -0700

    Implement code to solve problem

--- pramp/arrayIndexElementEquality.js
+++ pramp/arrayIndexElementEquality.js
@@ -23,27 +23,31 @@
 */
 
 // findEqualIndexElement takes in a sorted array arr
-
+var findEqualIndexElement = function (arr) {
   // begin set 0
-
+  var begin = 0;
   // end set to len of arr - 1
-
+  var end = arr.length - 1;
   // declare midpoint
-
+  var mid;
   // while begin <= end
-
+  while (begin <= end) {
     // mid = floor of begin + end / 2
-
+    mid = Math.floor(begin + end / 2);
     // if arr[mid] == mid
-
+    if (arr[mid] === mid) {
       // return mid
-
+      return mid;
     // if arr[mid] < mid
-
+    } else if (arr[mid] < mid) {
       // look at right half: set begin to mid + 1
-
+      begin = mid + 1;
     // if arr[mid] > mid
-
+    } else {
       // look at left half: set end to mid - 1
-      
+      end = mid - 1;
+    }
+  }
   // return -1 if all else fails
+  return -1;
+};

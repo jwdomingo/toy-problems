8549405b46bcb2200520260c5bd73e5e72791418
John Domingo <johnwill.domingo@gmail.com>
2016-05-03 09:52:06 -0700

    Pseudocode solution to problem

--- pramp/wordCountEngine.js
+++ pramp/wordCountEngine.js
@@ -26,3 +26,30 @@ I can then perform a stable quicksort on the array using the values and then the
 
 Time complexity would be O(n log n) and space would be O(n).
 */
+
+
+// runWordCountEngine takes in a text document
+
+  // declare wordCounts empty array
+
+  // return wordCounts if document has no length
+
+  // remove all special characters using RegEx: /[^\w\s]/gi and /[_]/gi
+
+  // split document by space and store into words array
+
+  // create empty object called hash
+
+  // loop through words
+
+    // insert new words into hash and init to 1
+
+    // increment existing words
+
+  // loop through hash and store each key-value pair in wordCounts
+
+  // sort wordCounts using native JavaScript sort method
+
+    // use index 1 of each tuple to compare whether or not a is less than b
+
+    // if tuples at index 1 are the same, return whether a is less than b using index 0

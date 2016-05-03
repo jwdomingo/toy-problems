3ffa1bef3b98fbfb72bb4940fa938e930e416b14
John Domingo <johnwill.domingo@gmail.com>
2016-05-03 10:55:21 -0700

    Clean code for optimal, non-recursive binary search solution

--- pramp/arrayIndexElementEquality.js
+++ pramp/arrayIndexElementEquality.js
@@ -15,39 +15,19 @@
  *      satisfies arr[i] = i
  */
 
-/*
-   input: sorted array of distinct integers
-   output: index of element where element is equal to its index or -1 if DNE
-   constraints: most efficient
-   edge cases: empty array => -1
-*/
-
-// findEqualIndexElement takes in a sorted array arr
 var findEqualIndexElement = function (arr) {
-  // begin set 0
   var begin = 0;
-  // end set to len of arr - 1
   var end = arr.length - 1;
-  // declare midpoint
   var mid;
-  // while begin <= end
   while (begin <= end) {
-    // mid = floor of begin + end / 2
     mid = Math.floor(begin + end / 2);
-    // if arr[mid] == mid
     if (arr[mid] === mid) {
-      // return mid
       return mid;
-    // if arr[mid] < mid
     } else if (arr[mid] < mid) {
-      // look at right half: set begin to mid + 1
       begin = mid + 1;
-    // if arr[mid] > mid
     } else {
-      // look at left half: set end to mid - 1
       end = mid - 1;
     }
   }
-  // return -1 if all else fails
   return -1;
 };
